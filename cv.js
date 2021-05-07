871a3ab06684d4886a274c3ccc18a32dd4710fdc
Lalenadr <lalena.deridder@student.intecbrussel.be>
2021-05-07 10:15:23 +0200

    template

--- cv.js
+++ cv.js
@@ -1,19 +1,31 @@
 let object = {
-    naam: "Lalena ",
+    naam: "Lalena",
     achternaam: "De Ridder",
     tussennamen: ["Sue", "Eloise"],
     sterrenbeeld: "schorpioen",
     verjaardag: 3,
-    verjaarmaand: " november",
-    woon: {"België" :["Dendermonde", "Bostroe"] },
-    food: {"koud" :["ijs", "toastjes"], "warm" :["pizza", "pasta"] }
+    verjaarmaand: "november",
+    woon: {
+        België :["Dendermonde", "Bostroe"] },
+    food: {
+        koud :["ijs", "toastjes"], 
+        warm :["pizza", "pasta"] },
+    bezigheden: {
+        creatief: ["tekenen", "muziek"],
+        rustig: ["Netflix", "lezen"]
+    }
 }
 
 
-console.log ("Mijn naam is " + object.naam + object.achternaam)
-console.log ("1 van mijn tussennamen is " + object.tussennamen[1])
-console.log("Ik woon in " + object.woon.België[1])
-console.log ("Geef mij maar " + object.food.koud[1])
+//console.log ("Mijn naam is " + object.naam + " " + object.achternaam)
+//console.log ("1 van mijn tussennamen is " + object.tussennamen[1])
+//console.log("Ik woon in " + object.woon.België[1])
+//console.log ("Geef mij maar " + object.food.koud[1])
+//let output = "ik ben jarig op " + object.verjaardag + " " + object.verjaarmaand
+//console.log(output);
 
-let output = "ik ben jarig op " + object.verjaardag + object.verjaarmaand
-console.log(output);
+output1 = `Mijn naam is ${object.naam} ${object.achternaam}. 
+Ik ben jarig op ${object.verjaardag} ${object.verjaarmaand}. 
+Enkele dingen die ik graag doe zijn ${object.bezigheden.rustig.toString()}`
+
+console.log(output1);
